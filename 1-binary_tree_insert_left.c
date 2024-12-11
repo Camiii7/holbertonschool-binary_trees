6fccd2d5aacf292762b4275ab61ac081608c6515
Camiii7 <camilairina7@gmail.com>
2024-12-11 19:01:13 +0000

    Declaramos variable para asignar memoria

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,15 +1,26 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 
+
+/**
+ * binary_tree_insert_left - Agregar un nodo a la izq
+ * @parent: Puntero al nodo padre
+ * @value: Valor a almacenar en el nuevo nodo
+ * Return: Puntero al nuevo nodo
+ */
+
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 		return (NULL);
-	
+
+	new_node = malloc(sizeof(binary_tree_t));
+
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
@@ -23,5 +34,4 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	return (new_node);
 
-	free(new_node);
 }
