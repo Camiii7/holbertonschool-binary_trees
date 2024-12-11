4aa558438e16e0eb3e3554b0f4598d03cb5cf8e7
Camiii7 <camilairina7@gmail.com>
2024-12-11 17:09:59 +0000

    Betty style

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,17 +3,19 @@
 
 
 /**
- * binary_tree_t - Crear un nodo de un arbol binario
- * @parent: Puntero al nodo padre 
+ * binary_tree_node - Crear un nodo de un arbol binario
+ * @parent: Puntero al nodo padre
  * @value: Valor en el nuevo nodo
- * Return: Puntero al nuevo nodo o NULL si falla 
+ * Return: Puntero al nuevo nodo o NULL si falla
  */
 
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
 	if (!new_node)
+
 		return (NULL);
 
 	new_node->n = value;
@@ -22,5 +24,4 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 
 	return (new_node);
-
 }
