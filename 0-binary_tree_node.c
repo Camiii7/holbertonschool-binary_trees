24f2c68f5a4aa909bd734cd55d7a0b2334b8b13d
Camiii7 <camilairina7@gmail.com>
2024-12-11 17:02:57 +0000

    Task 0 New node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_t - Crear un nodo de un arbol binario
+ * @parent: Puntero al nodo padre 
+ * @value: Valor en el nuevo nodo
+ * Return: Puntero al nuevo nodo o NULL si falla 
+ */
+
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+
+}
