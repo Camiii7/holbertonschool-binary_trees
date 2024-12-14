c4ee1d6b720a5316568910f52c5a6e64193216cb
Fer <fernando.falcon@holbertonstudents.com>
2024-12-14 14:56:24 +0000

    Correct Betty style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,10 +6,10 @@
  * Return: the size of the tree
  */
 
-size_t binary_tree_size (const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
