1113e63dc5d44c729d3c4e2c479da11207b52095
Fer <fernando.falcon@holbertonstudents.com>
2024-12-14 14:53:55 +0000

    Betty style

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_size - measure the size of a given tree
+ * @tree: a pointer to the tree to measure
+ * Return: the size of the tree
+ */
+
 size_t binary_tree_size (const binary_tree_t *tree)
 {
 	if (tree == NULL)
