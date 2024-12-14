92c2adeec03cfae9454933198a0329e0cb474279
Fer <fernando.falcon@holbertonstudents.com>
2024-12-14 15:33:20 +0000

    Betty style

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,11 +9,17 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t has_child;
+	size_t count = 0;
 
 	if (tree == NULL)
 		return (0);
 
 	has_child = (tree->left != NULL || tree->right != NULL) ? 1 : 0;
 
-	return (has_child + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	/* Arragement to be betty complaince - No line over 80 characters */
+	count = has_child;
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
+
+	return (count);
 }
